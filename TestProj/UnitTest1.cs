a716590707ba5937f1afac4fb3bf0e8cd5685cdb
Basavanagouda Patil <bpatil@microsoft.com>
2022-01-15 10:34:55 +0530

    commmit all

---
+++ TestProj/UnitTest1.cs
@@ -0,0 +1,36 @@
+using NUnit.Framework;
+
+namespace TestProj
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+            // Method intentionally left empty.
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+
+        [Test]
+        public void Test2()
+        {
+            Assert.Pass();
+        }
+        [Test]
+        public void Test3()
+        {
+            Assert.Pass();
+        }
+
+        [Test]
+        public void Test4()
+        {
+            Assert.Pass();
+        }
+    }
+}
